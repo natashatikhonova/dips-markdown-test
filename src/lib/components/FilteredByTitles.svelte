379e99b57004788ec704cff85a659b6cb11915b8
Sindre Rotnes Erichsen <sindreerichsen@gmail.com>
2022-07-07 18:07:59 +0200

    Fikset framvisning av overskrifter ved bruk trestruktur

--- src/lib/components/FilteredByTitles.svelte
+++ src/lib/components/FilteredByTitles.svelte
@@ -10,33 +10,47 @@
     
     $: if($documentList) {
             all_nodes = []
+
             $documentList.forEach((document) => {
-                // console.log(document)
-                let nodes_array = document.markdownTree.traverseBFS()
+                let nodes_array = document.markdownTree.get_nodes_in_order() //Return the nodes in the same order as it was read from file
+
+                // console.log("\nnodes_array: ")
+                // console.log(nodes_array)
+
                 nodes_array.forEach((node)=> {
                     all_nodes.push(node)
                 })
             })
+            // console.log("\n all_nodes: ")
+            // console.log(all_nodes)
             searched_titles_nodes = all_nodes.filter(item => (item.overskrift.toLowerCase().includes(searched_value.toLowerCase())) && item.id != 0);
-            // console.log(searched_titles_nodes)
-
+            console.log("\n searched_titles_nodes: ")
+            console.log(searched_titles_nodes)
+            
     }
 
      function make_titles_obj_list(obj_list, node_list){
-        let previous_node = {};
+        let previous_node = null;
         let element = {};
 
         for (let i = 0; i < node_list.length; i++){
             let node = node_list[i];
+            
             element = {node: node, show_title: false, show_children: false};
 
             if(previous_node == null){
                 element.show_title = true;
 
-            } else if(previous_node.object!= node.object){
+            } else if(previous_node.object != node.object){
                 element.show_title = true;
 
-            } else if (previous_node.children.includes(node)){
+            } else if ( (obj_list[obj_list.length-1].node.is_parent_of(node)) || (previous_node.id == node.parent.id)) { //If the last added element is parent of the current
+                    console.log(previous_node.overskrift + " er parent til " + node.overskrift)
+                    previous_node = node;
+                    continue;
+
+            }else if (previous_node.id == node.parent.id){
+                console.log(previous_node.overskrift + " er parent til " + node.overskrift)
                 previous_node = node;
                 continue;
             }
@@ -44,93 +58,74 @@
             obj_list.push(element)
             previous_node = node;
         }
-        
-        // console.log(obj_list)
+        console.log("\n titles_list_obj: ")
+        console.log(obj_list)
         return obj_list
     }
-
     
     $: titles_list_obj = make_titles_obj_list([], searched_titles_nodes)
-    $: console.log(titles_list_obj)
-
-    
 
      function show_children(obj){
          if(obj.node.children.length > 0) {
             // console.log(titles_list_obj)
 
             let new_titles_list_obj = []
+
             for(let i = 0; i < titles_list_obj.length; i++){
                 new_titles_list_obj.push(titles_list_obj[i])
     
                 if(titles_list_obj[i].node.id == obj.node.id) {//Adds the node's children to the list
     
                     if(obj.show_children == false) {
-                            // console.log("Legger til barn")
+                            console.log("Legger til barn")
                             obj.node.children.forEach((node) => {
                                 let newElement = {node: node, show_title: false, show_children: false}
                                 new_titles_list_obj.push(newElement)
+                                // console.log("\n new_titles_list_obj:")
+                                // console.log(new_titles_list_obj)
                             })
                             titles_list_obj[i].show_children = true;
     
-                        } else { //Removes the node's children
-                            // console.log("Fjerner barna")
-                           
-                            titles_list_obj[i].show_children = false;
+                    } else { //Removes the node's children
+                        console.log("Fjerner barna")
+                        
+                        titles_list_obj[i].show_children = false;
+                        let previous_obj = titles_list_obj[i];
 
-                            let previous_node = obj;
-                            i++;
+                        i++;
+                        while(i<titles_list_obj.length){ 
+                            console.log((previous_obj.show_children == false))
+                            console.log((previous_obj.node.is_parent_of(titles_list_obj[i].node)))
 
-                            while(i<titles_list_obj.length){ 
-                                if(titles_list_obj[i].node.parent.id == obj.node.id){
-                                    previous_node = titles_list_obj[i].node; 
-                                    titles_list_obj[i].show_children = false;
-                                    i++;
-                                }else{
-                                    if(titles_list_obj[i].node.parent.overskrift!="Root"){
-                                        previous_node = titles_list_obj[i].node; 
-                                        titles_list_obj[i].show_children = false;
-                                        i++;
-                                    }else{
-                                        break;
-                                    }
-                                }
+                            if( (previous_obj.show_children == false) && (previous_obj.node.is_parent_of(titles_list_obj[i].node)) ) {
+                                console.log("Hopper over " + titles_list_obj[i].node.overskrift)
+                                titles_list_obj[i].show_children = false;
+                                previous_obj = titles_list_obj[i]; 
+                                i++;
+                            } 
+                            else if (obj.node.is_parent_of(titles_list_obj[i].node)){
+                                console.log("Hopper over " + titles_list_obj[i].node.overskrift)
+                                titles_list_obj[i].show_children = false;
+                                previous_obj = titles_list_obj[i]; 
+                                i++;
+                            
+                            } 
+                            else{
+                                console.log("Break")
+                                break;
                             }
-                            i--;
-
-
-                            
-
-                            
-
-                            // let previous_node = null;
-                            // i++;
-                            // while(i < titles_list_obj.length){
-                            //     if ((titles_list_obj[i].node.parent.id == obj.node.id) && previous_node == null){
-                            //         console.log("Legger ikke til " + titles_list_obj[i].node.overskrift + " i listen")
-                            //         i++;
-                            //         previous_node = titles_list_obj[i].node;         
-                            //     }
-                            //     else if ((titles_list_obj[i].node.parent.id == obj.node.id)) { //If obj.node is parent of the node or if the previous_node is the parent of the node 
-                            //         console.log("Legger ikke til " + titles_list_obj[i].node.overskrift + " i listen")
-                            //         i++;
-                            //         previous_node = titles_list_obj[i].node;
-
-                            //     } else if (previous_node.id == titles_list_obj[i].node.parent.id){
-                            //         console.log("Legger ikke til " + titles_list_obj[i].node.overskrift + " i listen")
-                            //         i++;
-                            //         previous_node = titles_list_obj[i].node;
-                            //     } else {
-                            //         break
-                            //     }
-                            // }
-                            // i--
                         }
-                    
+                        i--;
+                        console.log("Etter while")
+                    }
+                            
                 }
+                    
             }
+
             titles_list_obj = new_titles_list_obj
-            // console.log(titles_list_obj)
+            console.log("\n NEW titles_list_obj: ")
+            console.log(titles_list_obj)
         }
     }
 
@@ -147,14 +142,15 @@
         <div>Ingen overskrifter</div>
     {:else}
 
-        
         {#each titles_list_obj as elementObj}
 
             {#if elementObj.show_title}
                 <div style="font-weight: bold">{(elementObj.node.object.date.toDateString() + ": " + elementObj.node.object.title)} </div>
             {/if}
+
             <div class="title" on:click={() => show_children(elementObj)}> 
                 {elementObj.node.format_string()} 
+
                 {#if elementObj.node.children.length>0}
 
                     {#if elementObj.show_children}
@@ -162,10 +158,12 @@
                     {:else }
                         <i class="material-icons">expand_more</i>
                     {/if}
+
                 {/if}
             </div>
             
         {/each} 
+
     {/if}
  
 

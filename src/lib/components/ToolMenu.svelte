19176c116adfce6b4307eb2d86e78ad8e81e1406
Torkild <torkild.finne@gmail.com>
2022-07-11 14:37:36 +0200

    ganske ferdig

--- src/lib/components/ToolMenu.svelte
+++ src/lib/components/ToolMenu.svelte
@@ -5,72 +5,72 @@
     //const documentTypes = ["Epikrise", "Poliklinisk notat", "Lab", "Sykepleier notat", "Rutinekontroll", "Poliklinisk notat", "Lab", "Sykepleier notat", "Rutinekontroll", "Poliklinisk notat", "Lab", "Sykepleier notat", "Rutinekontroll", "Poliklinisk notat", "Lab", "Sykepleier notat", "Rutinekontroll", "Poliklinisk notat", "Lab", "Sykepleier notat", "Rutinekontroll", "Poliklinisk notat", "Lab", "Sykepleier notat", "Rutinekontroll", "Poliklinisk notat", "Lab", "Sykepleier notat", "Rutinekontroll", "Poliklinisk notat", "Lab", "Sykepleier notat", "Rutinekontroll", "Poliklinisk notat", "Lab", "Sykepleier notat", "Rutinekontroll", "Poliklinisk notat", "Lab", "Sykepleier notat", "Rutinekontroll", "Poliklinisk notat", "Lab", "Sykepleier notat", "Rutinekontroll"];
     const documentTypes = ["Epikrise", "Poliklinisk notat", "Lab", "Sykepleier notat", "Rutinekontroll", "typ1", "typ2", "typ3", "typ4", "typ5", "typ6", "typ7", "typ8", "typ9", "typ10"];
 
+    //If the compleate menu is open
     let filterMenuOpen = false;
 
-    let newFilterMode = false;
+    //editmode make new edits and make new filter groups
+    let editMode = false;
 
-    let newFilterButtonName = "Nytt filter"
+    let modeButtonName = "Nytt filter"
 
+    let newFilterObj = null;
     let newFilterName ="";
-
     let newFilterGroup = documentTypes;
 
     let filter_searched_value = "";
-
     let filtergroup_searched_value ="";
 
     let allFilterMarked = true;
 
-    let newFilterObj = null;
-    
+    $currentFilterGroup = documentTypes
+
     $: searchedDocumentTypes = documentTypes.filter(item => (item.toLowerCase().includes(filter_searched_value.toLowerCase())));
 
     $: searchedFiltergroups = $myFilters.filter(item => (item.name.toLowerCase().includes(filtergroup_searched_value.toLowerCase())));
-
-
-
-    $currentFilterGroup = documentTypes
 
     const filterMenuHandler = () => {
         filterMenuOpen = !filterMenuOpen
     }
 
-
-    //Functions for adding new filtergroups
-    function clickedAll(){
-
-        if(newFilterGroup.length < documentTypes.length){
-            newFilterGroup = documentTypes
-        }
-        else{
-            newFilterGroup = []
-        }
-    }
-
-
-    $: if(newFilterGroup.length == documentTypes.length){
-        allFilterMarked = true
-    }
-    else if(newFilterGroup.length < documentTypes.length){
-        allFilterMarked = false
-    }
-
+    //Turns off all filters
     function turnOffFilter(){
         $noDocumentFilter = true
         $currentFilterGroup = documentTypes
     }
 
-    function newFilterClicked(){
+    
+    function modeChanger(){
         
-        newFilterMode = !newFilterMode
-        if(newFilterMode){
-            newFilterButtonName = "Dine filter"
+        editMode = !editMode
+        if(editMode){
+            modeButtonName = "Dine filter"
         }
         else{
-            newFilterButtonName = "Nytt filter"
+            modeButtonName = "Nytt filter"
         }
     }
 
+    //User clicked on edit and program swich mode with current crop as start point
+    function editItem(group){
+        newFilterObj = group
+        newFilterName = group.name
+        newFilterGroup = group.filters
+        modeChanger()
+    }
+
+    function addNewFilteMode(){
+        newFilterObj = null
+        newFilterName = ""
+        newFilterGroup = searchedDocumentTypes
+        modeChanger()
+    }
+
+    //shows current choosen filters in editmode
+    function showFilter(){
+        $currentFilterGroup = newFilterGroup
+    }
+
+    //Functions for adding new filtergroups
     function findNewId(){
         let ids = []
         $myFilters.forEach((filter)=>ids.push(filter.id))
@@ -79,6 +79,16 @@
             num += 1;
         }
         return num;
+    }
+
+    //If all button in editmode is clicked
+    function clickedAll(){
+        if(newFilterGroup.length < documentTypes.length){
+            newFilterGroup = documentTypes
+        }
+        else{
+            newFilterGroup = []
+        }
     }
 
     function saveFilter(){
@@ -92,206 +102,209 @@
         }    
         $myFilters = $myFilters
         $currentFilterGroup = newFilterObj.filters
-        newFilterClicked()
+        modeChanger()
     }
 
-    function showFilter(){
-        $currentFilterGroup = newFilterGroup
+    $: if(newFilterGroup.length == documentTypes.length){
+        allFilterMarked = true
+    }
+    else if(newFilterGroup.length < documentTypes.length){
+        allFilterMarked = false
     }
 
-    function editItem(group){
-        newFilterObj = group
-        newFilterName = group.name
-        newFilterClicked()
-    }
-
-    
 </script>
     <div class="filtermenu">
+
         <button on:click={filterMenuHandler} class:active={filterMenuOpen} class="dropdown-button" >Filter</button>
 
         <div class:show={filterMenuOpen} class="filtermenu-dropdown" >
-            {#if newFilterMode}
-            <div class="button-conteiner">
-                <button on:click={newFilterClicked}>{newFilterButtonName}</button>
-                <button on:click={showFilter}>Vis</button>
-                <button on:click={saveFilter}>Lagre</button>
-            </div>
+            
+        {#if editMode}
+        <div class="button-conteiner">
+            <button on:click={addNewFilteMode}>{modeButtonName}</button>
+            <button on:click={showFilter}>Vis</button>
+            <button on:click={saveFilter}>Lagre</button>
+        </div>
 
-                <input bind:value={newFilterName} type="text" placeholder="Filternavn..." name="search">
-                <input bind:value={filter_searched_value} type="text" placeholder="Søk.." name="search">
-                <label class="filterItem" style="border-bottom: 1px solid #666363">
-                    <input type="checkbox" on:click={clickedAll} bind:checked={allFilterMarked} value="alle" >
-                    Alle
-                    <span class="checkmark"></span>
-                </label>
-                
+            <input bind:value={newFilterName} type="text" placeholder="Filternavn..." name="search">
+            <input bind:value={filter_searched_value} type="text" placeholder="Søk.." name="search">
 
-                <div class= "filteroption-conteiner">
-            {#each searchedDocumentTypes as item}    
-                <label class="filterItem" >
+        <label class="filterItem" style="border-bottom: 1px solid #666363">
+            <input type="checkbox" on:click={clickedAll} bind:checked={allFilterMarked} value="alle" >
+            Alle
+            <span class="checkmark"></span>
+        </label>
+
+        <div class= "filteroption-conteiner">
+        {#each searchedDocumentTypes as item}    
+            <label class="filterItem" >
                 <input type="checkbox"  bind:group={newFilterGroup} value={item} >
                 {item}
                 <span class="checkmark"></span>
-                </label>
-            {/each}
-                </div>	
-
-            {:else}
-                    <button on:click={newFilterClicked}>{newFilterButtonName}</button>
-                    <h4>Dine filter:</h4>
-                    <input bind:value={filtergroup_searched_value} type="text" placeholder="Søk.." name="search" style="margin-bottom: 1vh">
-                    <div class="filterItem-button" class:active={$currentFilterGroup == documentTypes} on:click={() => $currentFilterGroup = documentTypes} value="alle">Vis allt</div>
-                    <div class= "myFilters-conteiner">
-                        {#each searchedFiltergroups as filter}
-                            <div class="filterItem-button" class:active={$currentFilterGroup == filter.filters} on:click={()=> $currentFilterGroup = filter.filters} value={filter.filters}>
-                                <t>{filter.name}</t>
-                                <div>
-                                    <button title ="Rediger" on:click={editItem(filter)}><i class="material-icons">edit</i></button>
-                                    <!-- class:visible={$currentlyAddingNewNote} on:click={editItem} -->
-                                    <button  on:click={()=> $myFilters = $myFilters.filter(item => (item.id != filter.id))} title="Slett" class="delete-button"><i class="material-icons">delete</i></button>
-                                </div>
-
-                            </div>
-                        {/each}
+            </label>
+        {/each}
+        </div>
+        	
+        {:else}
+            <button on:click={addNewFilteMode}>{modeButtonName}</button>
+            <h4>Dine filter:</h4>
+            <input bind:value={filtergroup_searched_value} type="text" placeholder="Søk.." name="search" style="margin-bottom: 1vh">
+            <div class="filterItem-button" class:active={$currentFilterGroup == documentTypes} on:click={() => $currentFilterGroup = documentTypes} value="alle">Vis allt</div>
+            <div class= "myFilters-conteiner">
+                {#each searchedFiltergroups as filter}
+                    <div class="filterItem-button" class:active={$currentFilterGroup == filter.filters} on:click={()=> $currentFilterGroup = filter.filters} value={filter.filters}>
+                        <t>{filter.name}</t>
+                        <div class="filteritem-buttons-conteiner">
+                            <button class="edit-buttons" title ="Rediger" on:click={editItem(filter)}><i class="material-icons">edit</i></button>
+                            <button class="edit-buttons" on:click={()=> $myFilters = $myFilters.filter(item => (item.id != filter.id))} title="Slett"><i class="material-icons">delete</i></button>
+                        </div>
                     </div>
-            {/if}
+                {/each}
+            </div>
+        {/if}
         </div>
         
         {#if $currentFilterGroup.length != documentTypes.length}
           <button class="filteroff-button" on:click={turnOffFilter}>Skru av filter</button>
-        {/if}	
+        {/if}
+
     </div>
 
 <style>
 
-    .filtermenu {
+.filtermenu {
     position: relative;
     display: inline-block;
+}
     
-    }
-        
-    .filtermenu-dropdown {
-        
-        display: none;
-        position: absolute;
-        background-color: #f6f6f6;
-        min-width: 230px;
-        border: 3px solid rgb(147, 147, 147);
-        z-index: 2;
-        max-height: 85vh;
-        height: fit-content;
-    }
+.filtermenu-dropdown {
+    
+    display: none;
+    position: absolute;
+    background-color: #f6f6f6;
+    min-width: 230px;
+    border: 3px solid rgb(147, 147, 147);
+    z-index: 2;
+    max-height: 85vh;
+    height: fit-content;
+}
 
-    .show {
+/* for filtermenu */
+.show {
     display:flex;
     flex-direction: column;
-    }
+}
 
-    .dropdown-button {
-        display: inline-flex;
-        align-items: center;
-        margin: 0.5vh;
-        margin-bottom: 0vh;
-        margin-right: 2vh;
-        height:4vh;
-        transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
-        background: #d43838;
-        color:#ffffff;
-        padding: 16px;
-        font-size: 16px;
-        cursor: pointer;
-        border: solid 0.1em rgb(255, 92, 81 ,0);
-        box-shadow: 0 0 0 0.2rem rgb(255, 92, 81, 0);
-    }
-        
-    .dropdown-button:hover, .dropdown-button.active {
+.dropdown-button {
+    display: inline-flex;
+    align-items: center;
+    margin: 0.5vh;
+    margin-bottom: 0vh;
+    margin-right: 2vh;
+    height:4vh;
+    transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+    background: #d43838;
+    color:#ffffff;
+    padding: 16px;
+    font-size: 16px;
+    cursor: pointer;
+    border: solid 0.1em rgb(255, 92, 81 ,0);
+    box-shadow: 0 0 0 0.2rem rgb(255, 92, 81, 0);
+}
+    
+.dropdown-button:hover, .dropdown-button.active {
     color:#ffffff;
     border: solid 0.1em;
     box-shadow: 0 0 0 0.2rem rgb(255, 92, 81);
-    }
+}
 
-    .button-conteiner{
-        display:flex;
-        flex-direction: row;
-        justify-content:space-between;
-        margin: 3px;
-    }
+.button-conteiner{
+    display:flex;
+    flex-direction: row;
+    justify-content:space-between;
+    margin: 3px;
+}
 
-    .button-conteiner button{
-        width: 100%;
-        margin: 1px;
-    }
+.button-conteiner button{
+    width: 100%;
+    margin: 1px;
+}
 
-    input{
-        margin: 3px;
-    }
+input{
+    margin: 3px;
+}
 
-    .filterItem {
+.filterItem {
     color: black;
     padding: 12px 16px;
     text-decoration: none;
     display: block;
-    }	
-        
-    .filterItem:hover {
-        background-color: #ddd
-    }
+}	
+    
+.filterItem:hover {
+    background-color: #ddd
+}
 
-    .filteroff-button{
-        display: inline-flex;
-        align-items: center;
-        border: none;
-        background: none;
-        height:4vh;
-        transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
-        cursor: pointer;
+.filterItem-button{
+    display: flex;
+    flex-direction: row;
+    justify-content:space-between;
+    align-items: center;
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    border-bottom:  1px solid #666363;
+}
 
-    }
-    .filteroff-button:hover {
-        color:#666363;
-    }
+.filterItem-button:hover {
+    background-color: #e6f2ff;
+}
 
-    .filterItem-button{
-        display: flex;
-        flex-direction: row;
-        justify-content:space-between;
-        align-items: center;
-        color: black;
-        padding: 12px 16px;
-        text-decoration: none;
-        border-bottom:  1px solid #666363;
-    }
+.filterItem-button.active{
+    background-color: #ccebff;
+}
 
-    .filterItem-button:hover {
-        background-color: #e6f2ff;
-    }
+.filteritem-buttons-conteiner{
+    display: flex;
+    flex-direction: row;
+}
 
-    .filterItem-button.active{
-        background-color: #ccebff;
-    }
+.myFilters-conteiner{
+    display: flex;
+    flex-direction: column;
+    max-height:60vh;
+    overflow-y: auto;
 
-    .myFilters-conteiner{
-        display: flex;
-        flex-direction: column;
-        max-height:60vh;
-        overflow-y: auto;
+}
+.filteroption-conteiner{
+    max-height: 60vh;
+    overflow-y: auto;
+}
 
-    }
-    .filteroption-conteiner{
-        max-height: 60vh;
-        overflow-y: auto;
-    }
+.edit-buttons {
+    align-self: center;
+    background: none;
+    border:none;
+    transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+    cursor: pointer;
+}
 
-    .delete-button {
-        align-self: center;
-        background: none;
-        border:none;
-        transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
-        cursor: pointer;
-  }
-
-  .delete-button:hover {
+.edit-buttons:hover {
     color: #d43838;
-  }
+}
+
+.filteroff-button{
+    display: inline-flex;
+    align-items: center;
+    border: none;
+    background: none;
+    height:4vh;
+    transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+    cursor: pointer;
+
+}
+.filteroff-button:hover {
+    color:#666363;
+}
+
 </style>
